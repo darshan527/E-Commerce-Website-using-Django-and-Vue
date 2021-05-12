d83086175e6378bf30746f9ea8b2091999fc5987
darshan527 <darshan.hosamani6@gmail.com>
2021-05-12 17:09:58 +0530

    Vue App

--- djackets_vue/src/App.vue
+++ djackets_vue/src/App.vue
@@ -1,7 +1,56 @@
 <template>
- <div id="wrapper">
-  <router-view/>
- </div>
+  <div id="wrapper">
+    <nav class="navbar is-dark">
+      <div class="navbar-brand">
+        <router-link to="/" class="navbar-item">
+          <strong>Djackets</strong>
+        </router-link>
+        <a
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbar-menu"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu" id="navbar-menu">
+        <div class="navbar-end">
+          <router-link to="/summer" class="navbar-item">
+            <strong>Summer</strong>
+          </router-link>
+          <router-link to="/winter" class="navbar-item">
+            <strong>Winter</strong>
+          </router-link>
+          <router-link to="/spring" class="navbar-item">
+            <strong>Spring</strong>
+          </router-link>
+
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link to="/log-in" class="button is-light">
+                Log In
+              </router-link>
+              <router-link to="/cart" class="button is-light">
+                <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+                <span>Cart</span>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <section class="section">
+      <router-view />
+    </section>
+
+    <footer class="footer">
+      <p class="has-text-centered">Copyright (c) 2021</p>
+    </footer>
+  </div>
 </template>
 
 <style lang="scss">
