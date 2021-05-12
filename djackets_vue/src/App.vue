75c2d4d90557785f761f0a6de9ec464a6d4fe6b7
darshan527 <darshan.hosamani6@gmail.com>
2021-05-12 23:04:02 +0530

    wip

--- djackets_vue/src/App.vue
+++ djackets_vue/src/App.vue
@@ -10,13 +10,18 @@
           aria-label="menu"
           aria-expanded="false"
           data-target="navbar-menu"
+          @click="showMobileMenu = !showMobileMenu"
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
-      <div class="navbar-menu" id="navbar-menu">
+      <div
+        class="navbar-menu"
+        id="navbar-menu"
+        v-bind:class="{ 'is-active': showMobileMenu }"
+      >
         <div class="navbar-end">
           <router-link to="/summer" class="navbar-item">
             <strong>Summer</strong>
@@ -33,7 +38,7 @@
               <router-link to="/log-in" class="button is-light">
                 Log In
               </router-link>
-              <router-link to="/cart" class="button is-light">
+              <router-link to="/cart" class="button is-success">
                 <span class="icon"><i class="fas fa-shopping-cart"></i></span>
                 <span>Cart</span>
               </router-link>
@@ -52,6 +57,16 @@
     </footer>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+    };
+  },
+};
+</script>
 
 <style lang="scss">
 @import "../node_modules/bulma";
