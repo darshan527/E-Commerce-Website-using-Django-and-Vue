d6f939550de184c5cba1d4f2066bb207322e7e4a
darshan527 <darshan.hosamani6@gmail.com>
2021-05-13 20:16:55 +0530

    Added Category Model

--- djackets_django/product/models.py
+++ djackets_django/product/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('name', )
+
+    def __str__(self):
+        return self.name
+
+    def get_abolute_url(self):
+        return f'/{self.slug}/'
